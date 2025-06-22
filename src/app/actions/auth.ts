586d5c06a8c8e21ef30a822b47f03e42e58bca80
Victor Gonzalez <dazedbutamused@gmail.com>
2025-06-21 19:06:20 -0700

    set up pass hashing

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,13 +1,33 @@
+import { argon2id, argon2Verify } from 'hash-wasm';
+import { randomBytes } from 'crypto';
 import { signupSchema, SignupSchema } from '@/lib/definitions';
+import config from '@/config/index'
+
+const ARGON2ID_CONFIG = {
+  // OWASP recommended config
+  iterations: 2,
+  parallelism: 1,
+  memorySize: 1024 * 19, // 19 MiB
+  hashLength: 32,
+  outputType: 'encoded' as const,
+  secret: config.db.pepper,
+};
+
+const hashPassword = (password: string): Promise<string> =>
+  argon2id({
+    ...ARGON2ID_CONFIG,
+    password,
+    salt: randomBytes(16),
+  });
+
+export const authenticate = (password: string, hash: string) =>
+  argon2Verify({ password, hash, secret: config.db.pepper });
 
 export async function signup(formData: SignupSchema) {
-  formData.password = 'foo';
   const validatedFields = signupSchema.safeParse(formData);
   if (!validatedFields.success) {
-    console.log('faaaill');
     return {
       errors: validatedFields.error.flatten().fieldErrors,
     };
   }
-  console.log('hiiitsss');
 }
---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+const config = {
+  db: {
+    pepper: process.env.PEPPER,
+  },
+};
+
+export default config;
