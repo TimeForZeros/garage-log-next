9302a1f374c50d8fa40295076a0a9a931189d134
Victor Gonzalez <dazedbutamused@gmail.com>
2025-06-24 17:29:26 -0700

    seed file

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient, Prisma } from "../src/app/generated/prisma";
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: "test",
+    email: "test@example.io",
+    password: 'foobarfizz',
+  },
+];
+
+export async function main() {
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+
+main();
