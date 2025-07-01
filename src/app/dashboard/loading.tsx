f563503f64f994e4721786c4ab2ebad1d0efa6a5
Victor Gonzalez <dazedbutamused@gmail.com>
2025-06-30 18:19:33 -0700

    placeholder loading stuff

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from '@/components/ui/skeleton';
+const LoadingElement = () => (
+  <div className='flex items-center space-x-4 py-1'>
+    <Skeleton className='h-12 w-12 rounded-full' />
+    <div className='space-y-2'>
+      <Skeleton className='h-4 w-[250px]' />
+      <Skeleton className='h-4 w-[200px]' />
+    </div>
+  </div>
+);
+export default function Loading() {
+  return (
+    <div>
+      <LoadingElement />
+      <LoadingElement />
+      <LoadingElement />
+      <LoadingElement />
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,28 @@
-export default function Dashboard() {
-  
+import { Suspense } from 'react';
+import Loading from './loading';
+
+const wait = async (ms: number): Promise<void> => new Promise((res) => setTimeout(() => res(), ms));
+const Posts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+  await wait(2000);
+  return (
+    <ul>
+      {posts.map((post: any) => (
+        <li>{post.title}</li>
+      ))}
+    </ul>
+  );
+};
+export default async function Dashboard() {
+  return (
+    <div>
+      <div>Hello World</div>
+      <div>
+        <Suspense fallback={<Loading />}>
+          <Posts />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
